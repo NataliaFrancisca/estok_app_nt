1884d9ce740461a58995be0371062cc1e4beb3da
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-16 20:04:50 -0300

    feat: add color for the stock status

--- lib/colors.dart
+++ lib/colors.dart
@@ -6,4 +6,7 @@ class AppColors{
   static Color primaryColor = const Color(0xFF58355E);
   static Color iconPrimaryColor = const Color(0xFFC4C4C4);
   static Color loginButtonTextColor = const Color(0xFF463E47);
+  static Color yellowStatusColor = const Color(0xFFDCC707);
+  static Color greenStatusColor = const Color(0xFF3AA637);
+  static Color redStatusColor = const Color(0xFFA63737);
 }
