dfb161ed38684402653fa857a1937c4fd480a924
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-21 19:27:36 -0300

    feat: change the stateless to stateful; use the scopeModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:estok_app_natalia_francisca/colors.dart';
+import 'package:estok_app_natalia_francisca/models/user_model.dart';
 import 'package:estok_app_natalia_francisca/ui/pages/home_page.dart';
+import 'package:estok_app_natalia_francisca/ui/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(UserModel()));
 }
 
-class MyApp extends StatelessWidget with AppColors{
+class MyApp extends StatefulWidget with AppColors{
+  final UserModel userScopedModel;
+
+  MyApp(this.userScopedModel);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Estok App',
-      theme: ThemeData(
-        primaryColor: AppColors.primaryColor,
-        accentColor: AppColors.primaryColor,
+    return ScopedModel(
+      model: widget.userScopedModel,
+        child: MaterialApp(
+        title: 'Estok App',
+        theme: ThemeData(
+          primaryColor: AppColors.primaryColor,
+          accentColor: AppColors.primaryColor,
+        ),
+          debugShowCheckedModeBanner: false,
+        home: LoginPage()
       ),
-        debugShowCheckedModeBanner: false,
-      home: HomePage()
     );
   }
 }
