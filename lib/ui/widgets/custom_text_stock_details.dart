222d488626cd9ffb2c4a05a8049a772e63e1db94
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-22 20:06:11 -0300

    feat: create a custom text to the stock details

---
+++ lib/ui/widgets/custom_text_stock_details.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStockDetails extends StatelessWidget {
+  final String _text;
+  CustomTextStockDetails(this._text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 11),
+      child: Text(
+        "${this._text}",
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          fontFamily: "Montserrat"
+        )
+      ),
+    );
+  }
+}
