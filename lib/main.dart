99f87027737d4b4ede7a61defc1480d2929d9b6d
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-21 21:43:53 -0300

    feat: add the stock scopedMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:estok_app_natalia_francisca/colors.dart';
+import 'package:estok_app_natalia_francisca/models/stock_model.dart';
 import 'package:estok_app_natalia_francisca/models/user_model.dart';
 import 'package:estok_app_natalia_francisca/ui/pages/home_page.dart';
 import 'package:estok_app_natalia_francisca/ui/pages/login_page.dart';
@@ -6,13 +7,14 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 void main() {
-  runApp(MyApp(UserModel()));
+  runApp(MyApp(UserModel(), StockModel()));
 }
 
 class MyApp extends StatefulWidget with AppColors{
   final UserModel userScopedModel;
+  final StockModel stockScopedModel;
 
-  MyApp(this.userScopedModel);
+  MyApp(this.userScopedModel, this.stockScopedModel);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -23,15 +25,18 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return ScopedModel(
       model: widget.userScopedModel,
-        child: MaterialApp(
-        title: 'Estok App',
-        theme: ThemeData(
-          primaryColor: AppColors.primaryColor,
-          accentColor: AppColors.primaryColor,
-        ),
-          debugShowCheckedModeBanner: false,
-        home: LoginPage()
+        child: ScopedModel(
+          model: widget.stockScopedModel,
+          child: MaterialApp(
+            title: 'Estok App',
+            theme: ThemeData(
+              primaryColor: AppColors.primaryColor,
+              accentColor: AppColors.primaryColor,
+            ),
+              debugShowCheckedModeBanner: false,
+            home: LoginPage()
       ),
+        ),
     );
   }
 }
