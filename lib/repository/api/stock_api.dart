a9c70facad3a4a574ec0c5ca1221d5f2d2d27da3
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-22 22:17:03 -0300

    feat: a test code

--- lib/repository/api/stock_api.dart
+++ lib/repository/api/stock_api.dart
@@ -2,12 +2,18 @@ import 'dart:convert';
 
 import 'package:estok_app_natalia_francisca/entities/stock.dart';
 import 'package:estok_app_natalia_francisca/entities/user.dart';
+import 'package:estok_app_natalia_francisca/repository/api/user_api.dart';
 import 'package:estok_app_natalia_francisca/repository/local/user_repository.dart';
+import 'package:estok_app_natalia_francisca/ui/pages/home_page.dart';
+import 'package:estok_app_natalia_francisca/ui/pages/login_page.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class StockApi{
   static final StockApi instance = StockApi._();
   StockApi._();
+
+  BuildContext context;
 
   Future<List<Stock>> getAll(type) async{
     List<Stock> stock;
@@ -17,7 +23,6 @@ class StockApi{
       User user = await UserRepository.instance.getUsuario();
       String authorization = 'Bearer ${user.token}';
 
-
       var response = await http.get(url,
         headers: {
           'Content-type': 'application/json',
@@ -25,6 +30,13 @@ class StockApi{
         },
       );
 
+      if(response.statusCode == 403 || response.statusCode == 401){
+        return Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context){
+            return LoginPage();
+        }));
+      }
+ 
+   
       if(response.statusCode != 200){
         return null;
       }
@@ -39,7 +51,7 @@ class StockApi{
 
       return stock;
     } on Exception catch(error){
-      print('Algo de errado na API');
+      print('Algo de errado na API $error');
       return null;
     }
   }
