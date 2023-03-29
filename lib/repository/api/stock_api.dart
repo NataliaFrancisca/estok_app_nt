5e6632b43e3c68bbd08f7a12f7a5ad483c02e4ad
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-28 21:40:46 -0300

    feat: function to delete the stock

--- lib/repository/api/stock_api.dart
+++ lib/repository/api/stock_api.dart
@@ -2,11 +2,8 @@ import 'dart:convert';
 
 import 'package:estok_app_natalia_francisca/entities/stock.dart';
 import 'package:estok_app_natalia_francisca/entities/user.dart';
-import 'package:estok_app_natalia_francisca/repository/api/user_api.dart';
+import 'package:estok_app_natalia_francisca/repository/api/product_api.dart';
 import 'package:estok_app_natalia_francisca/repository/local/user_repository.dart';
-import 'package:estok_app_natalia_francisca/ui/pages/home_page.dart';
-import 'package:estok_app_natalia_francisca/ui/pages/login_page.dart';
-import 'package:estok_app_natalia_francisca/ui/utils/filter_stock.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -30,23 +27,22 @@ class StockApi{
           'Authorization': authorization
         },
       );
-   
+
       if(response.statusCode != 200){
         return null;
       }
 
       var responseData = json.decode(utf8.decode(response.bodyBytes));
       var data = responseData["data"];
+      print('RESPONSE[STOCK] $responseData');
 
       stock = (data as List)?.map((json){
         return Stock.fromJson(json as Map<String, dynamic>);
       })?.toList();
 
-
-
       return stock;
     } on Exception catch(error){
-      print('Algo de errado na API $error');
+      print('Algo de errado na API');
       return null;
     }
   }
@@ -84,6 +80,30 @@ class StockApi{
     }
   }
 
+   Future<Map<String, dynamic>>delete(int id) async{
+    try{
+      String url = 'http://54.90.203.92/estoques/$id';
+      User user = await UserRepository.instance.getUsuario();
+      String authorization = 'Bearer ${user.token}';
+
+      var response = await http.delete(url,
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization': authorization
+        }
+      );
+
+      print('RESPONSE[STATUS] ${response.statusCode}');
 
 
+      if(response.statusCode == 200){
+        var responseData = json.decode(utf8.decode(response.bodyBytes));
+        return responseData;
+      }else{
+        return null;
+      }
+    } on Exception catch(error){
+      return null;
+    }
+  }
 }
