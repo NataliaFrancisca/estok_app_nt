f66d834c362b7581e053aa469e7c03d37a2ebc1f
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-28 21:40:15 -0300

    feat: model to delete the stock

--- lib/models/stock_model.dart
+++ lib/models/stock_model.dart
@@ -1,7 +1,6 @@
 import 'package:estok_app_natalia_francisca/entities/stock.dart';
+import 'package:estok_app_natalia_francisca/repository/api/product_api.dart';
 import 'package:estok_app_natalia_francisca/repository/api/stock_api.dart';
-import 'package:estok_app_natalia_francisca/repository/local/stock_repository.dart';
-import 'package:estok_app_natalia_francisca/ui/utils/filter_stock.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -30,6 +29,16 @@ class StockModel extends Model{
       onSuccess();
     }else{
       onFail('Algo deu errado ao tentar salvar o estoque');
+    }
+  }
+
+  void deleteStock(int id, {VoidCallback onSuccess, VoidCallback onFail(String message)}) async{
+    var stockDelete = await StockApi.instance.delete(id);
+
+    if(stockDelete != null){
+      onSuccess();
+    }else{
+      onFail("Algo de eraddo na hora de deletar o estoque");
     }
   }
 
