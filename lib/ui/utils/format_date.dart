90566c2f0c5f0e9088ed95178ec0b982c93fe55d
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-30 20:09:41 -0300

    feat: function to transform a string to datetime

--- lib/ui/utils/format_date.dart
+++ lib/ui/utils/format_date.dart
@@ -1,4 +1,11 @@
 String tranformDate(DateTime date) {
   var month = date.month < 10 ? "0${date.month}" : date.month;
-  return "${date.day}/$month/${date.year}";
+  var day = date.day < 10 ? "0${date.day}" : date.day;
+  return "$day/$month/${date.year}";
+}
+
+DateTime tranformStringDate(String date){
+  var arr = date.split('/').reversed.join('-');
+  print(arr);
+  return DateTime.parse(arr);
 }
