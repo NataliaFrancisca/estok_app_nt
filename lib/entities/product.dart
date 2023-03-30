32f14636190f8d507cb6b23fd1f543741af32ff7
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-29 21:23:44 -0300

    feat: fix the problem with num values, and add toJsonRequest

--- lib/entities/product.dart
+++ lib/entities/product.dart
@@ -3,7 +3,7 @@ class Product{
   int estoque_id;
   String nome;
   String descricao;
-  String image;
+  String imagem;
   double valor_item;
   double valor_unitario;
   int quantidade;
@@ -14,7 +14,7 @@ class Product{
     this.estoque_id,
     this.nome,
     this.descricao,
-    this.image,
+    this.imagem,
     this.valor_item,
     this.valor_unitario,
     this.quantidade,
@@ -23,14 +23,14 @@ class Product{
 
   factory Product.fromJson(Map<String, dynamic> json){
     return Product(
-      id: (json['id'] as num).toInt(),
-      estoque_id: (json['estoque_id'] as num).toInt(),
+      id: (json['id'] != null) ? (json['id'] as num).toInt() : null,
+      estoque_id: (json['estoque_id'] != null) ? (json['estoque_id'] as num).toInt() : null,
       nome: (json['nome'] as String),
       descricao: (json['descricao'] as String),
-      image: (json['image'] as String),
-      valor_item: (json['valor_item'] as num).toDouble(),
-      valor_unitario: (json['valor_unitario'] as num).toDouble(),
-      quantidade: (json['quantidade'] as num).toInt(),
+      imagem: (json['image'] as String),
+      valor_item: (json['valor_item'] != null) ? (json['valor_item'] as num).toDouble() : null,
+      valor_unitario: (json['valor_unitario'] != null) ? (json['valor_unitario'] as num).toDouble() : null,
+      quantidade: (json['quantidade'] != null) ? (json['quantidade'] as num).toInt() : null,
       site: (json['site'] as String),
     );
   }
@@ -41,7 +41,19 @@ class Product{
       "estoque_id": this.estoque_id,
       "nome": this.nome,
       "descricao": this.descricao,
-      "image": this.image,
+      "imagem": this.imagem,
+      "valor_item": this.valor_item,
+      "valor_unitario": this.valor_unitario,
+      "quantidade": this.quantidade,
+      "site": this.site
+    };
+  }
+
+  Map<String, dynamic> toJsonRequest(){
+    return <String, dynamic>{
+      "nome": this.nome,
+      "descricao": this.descricao,
+      "imagem": this.imagem,
       "valor_item": this.valor_item,
       "valor_unitario": this.valor_unitario,
       "quantidade": this.quantidade,
