a56602b7269e2e3da1b259578685b61070c9ed1f
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-29 21:23:56 -0300

    feat: function to add the product

--- lib/models/product_stock_model.dart
+++ lib/models/product_stock_model.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:estok_app_natalia_francisca/entities/product.dart';
 import 'package:estok_app_natalia_francisca/repository/api/product_api.dart';
+import 'package:estok_app_natalia_francisca/repository/api/upload_api.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -23,6 +24,35 @@ class ProductStockModel extends Model{
     setState();
     this.futureProduct = ProductApi.instance.getAll(id);
     setState();
+  }
+
+  void addProduct(Product product, {VoidCallback onSuccess, VoidCallback onFail(String message)}) async{
+    if(file != null){
+      String urlImage = await uploadImageFile(this.file);
+  
+      if(urlImage != null){
+        product.imagem = urlImage;
+        file = null;
+      }else{
+        onFail('Erro ao adicionar a imagem do produto');
+        return;
+      }
+    }
+
+    Product productSaved = await ProductApi.instance.save(product);
+
+    if(productSaved != null){
+      product = productSaved;
+      onSuccess();
+    }else{
+      onFail('Erro ao adicionar o produto');
+    }
+
+    setState();
+  }
+
+  Future<String> uploadImageFile(File file) async{
+    return await UploadApi.instance.upload(file);
   }
 }
 
