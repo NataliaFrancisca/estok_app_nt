fe3e528563ec02296adaa6d035e6d077f24a5fca
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-01 10:16:45 -0300

    feat: create historic tile

---
+++ lib/ui/tile/historic_tile.dart
@@ -0,0 +1,85 @@
+import 'package:estok_app_natalia_francisca/colors.dart';
+import 'package:flutter/material.dart';
+
+class HistoricTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 6),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'INSERÇÃO PRODUTO OMO',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w700
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+
+                      Text(
+                        'PRODUTO',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          color: new Color(0xFF949191)
+                        )
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '09/10/2022',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.primaryColor
+                        )
+                      ),
+
+                      Text(
+                        '08:20',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+          SizedBox(height: 20),
+
+          Divider(
+            height: 0,
+            thickness: 1,
+            indent: 0,
+            endIndent: 0,
+            color: new Color(0xFFBEBBBB),
+          ),
+        ]
+      ),
+    );
+  }
+}
