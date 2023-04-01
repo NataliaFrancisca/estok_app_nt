34824e6bcc7ac9b66b62055ecd41bc08a7f60e0f
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-01 10:17:01 -0300

    feat: create historic page

--- lib/ui/pages/historic_page.dart
+++ lib/ui/pages/historic_page.dart
@@ -2,7 +2,9 @@ import 'package:estok_app_natalia_francisca/colors.dart';
 import 'package:estok_app_natalia_francisca/models/user_model.dart';
 import 'package:estok_app_natalia_francisca/ui/pages/home_page.dart';
 import 'package:estok_app_natalia_francisca/ui/pages/perfil_page.dart';
+import 'package:estok_app_natalia_francisca/ui/tile/historic_tile.dart';
 import 'package:estok_app_natalia_francisca/ui/utils/logout_function.dart';
+import 'package:estok_app_natalia_francisca/ui/widgets/custom_bottom_nav_bar.dart';
 import 'package:estok_app_natalia_francisca/ui/widgets/custom_navigation_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -14,22 +16,6 @@ class HistoricPage extends StatefulWidget {
 
 class _HistoricPageState extends State<HistoricPage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-   int _selectedIndex = 1;
-
-  static List _widgetOptions = <Widget>[
-    HomePage(),
-    HistoricPage(),
-    PerfilPage(),
-  ];
-
-  @override
-  void _onItemTapped(int index) {
-    setState(() {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context){
-        return _widgetOptions[index];
-      }));
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,36 +30,24 @@ class _HistoricPageState extends State<HistoricPage> {
             fontSize: 15
           ),
         ),
-      ),
-      
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedLabelStyle: TextStyle(
-          color: new Color(0xFF909FAD),
-        ),
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.playlist_add),label: 'Historico'),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded),label: 'Perfil'),
-        ],
-
-        currentIndex: _selectedIndex,
-        selectedItemColor: AppColors.primaryColor,
-        onTap: _onItemTapped,
+        centerTitle: true,
+        backgroundColor: new Color(0xFFF7F2F8),
       ),
 
+      bottomNavigationBar: CustomBottomNavBar('historic_page'),
+    
       body: Center(
         child: 
           ScopedModelDescendant(
             builder: (BuildContext context, Widget child, UserModel userModel){
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Column(
-                    children: [
-                      Text("HISTORICO")
-                    
-                    ],
-        
-                ),
+              return Center(
+                child: ListView(
+                  children: [
+                    SizedBox(height: 20),
+                    HistoricTile(),
+                    HistoricTile(),
+                  ],
+                )
               );
             }
           )
