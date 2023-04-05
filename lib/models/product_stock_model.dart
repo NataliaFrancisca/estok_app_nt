36b191146af5f98d80837a160f6c010518247504
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-04 22:53:41 -0300

    feat: function to delete the stock and the products

--- lib/models/product_stock_model.dart
+++ lib/models/product_stock_model.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:estok_app_natalia_francisca/entities/product.dart';
+import 'package:estok_app_natalia_francisca/entities/stock.dart';
 import 'package:estok_app_natalia_francisca/repository/api/product_api.dart';
+import 'package:estok_app_natalia_francisca/repository/api/stock_api.dart';
 import 'package:estok_app_natalia_francisca/repository/api/upload_api.dart';
+import 'package:estok_app_natalia_francisca/ui/utils/format_date.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -26,7 +29,8 @@ class ProductStockModel extends Model{
     setState();
   }
 
-  void addProduct(Product product, {VoidCallback onSuccess, VoidCallback onFail(String message)}) async{
+  void addProduct(Product product, Stock stock, {VoidCallback onSuccess, VoidCallback onFail(String message)}) async{
+    
     if(file != null){
       String urlImage = await uploadImageFile(this.file);
   
@@ -40,8 +44,9 @@ class ProductStockModel extends Model{
     }
 
     Product productSaved = await ProductApi.instance.save(product);
+    Stock updatedStock = await StockApi.instance.update(updateStockElements(stock, product));
 
-    if(productSaved != null){
+    if(productSaved != null && updatedStock != null){
       product = productSaved;
       onSuccess();
     }else{
@@ -54,6 +59,14 @@ class ProductStockModel extends Model{
   Future<String> uploadImageFile(File file) async{
     return await UploadApi.instance.upload(file);
   }
+
+  Stock updateStockElements(Stock stock, Product product){
+    stock.quantidade_total = stock.quantidade_total + product.quantidade;
+    stock.data_entrada = removeTheUTC(stock.data_entrada);
+    stock.data_validade = removeTheUTC(stock.data_validade);
+    return stock;
+  }
+
 }
 
 
