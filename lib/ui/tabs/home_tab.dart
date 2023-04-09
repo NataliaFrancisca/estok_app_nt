dcc0ddc91b357571f6296da8803047af6895cd3a
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-09 18:21:19 -0300

    feat: home tab using the filter tab

--- lib/ui/tabs/home_tab.dart
+++ lib/ui/tabs/home_tab.dart
@@ -20,81 +20,79 @@ class _HomeTabState extends State<HomeTab> {
 
   void initState() {
     super.initState();
-    _reload();
+    StockModel.of(context).filterStock(widget.type);
   }
 
   void _reload() {
-    StockModel.of(context).fetch();
+    StockModel.of(context).fetch(widget.type);
   }
   
   @override
   Widget build(BuildContext context) {
+  //   return ScopedModelDescendant<StockModel>(builder: (context, child, stockModel){
+  //     return FutureBuilder(
+  //       future: stockModel.futureStock,
+  //       builder: (BuildContext context, AsyncSnapshot<List<Stock>> snapshot){
+  //           if(snapshot.hasError){
+  //             return Message.alert("Não foi possível obter os dados do servidor");
+  //           } else if(!snapshot.hasData){
+  //             return Message.alert("Não foi possivel obter os dados dos estoques");
+  //           }else if(snapshot.data.isEmpty){
+  //             return Message.alert("Nenhum estoque encontrado", fontSize: 16);
+  //           }else{
+  //             return RefreshIndicator(
+  //               onRefresh: () async{
+  //                 this._reload();
+  //               },
+  //               child: ListView.builder(
+  //                 padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+  //                 itemCount: snapshot.data.length,
+  //                 itemBuilder: (BuildContext context, int index){
+  //                   return StockTile(snapshot.data[index]);
+  //                 },
+  //               ),
+  //             );
+  //           }
+  //         } 
+  //       );
+  //     },
+  //   );
+  // }
+
     return ScopedModelDescendant<StockModel>(builder: (context, child, stockModel){
       return FutureBuilder(
-        future: stockModel.futureStock,
+        future: stockModel.filteredStock,
         builder: (BuildContext context, AsyncSnapshot<List<Stock>> snapshot){
-            if(snapshot.hasError){
-              return Message.alert("Não foi possível obter os dados do servidor");
-            } else if(!snapshot.hasData){
-              return Message.alert("Não foi possivel obter os dados dos estoques");
-            }else if(snapshot.data.isEmpty){
-              return Message.alert("Nenhum estoque encontrado", fontSize: 16);
-            }else{
-              return RefreshIndicator(
-                onRefresh: () async{
-                  this._reload();
-                },
-                child: ListView.builder(
-                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index){
-                    return StockTile(snapshot.data[index]);
-                  },
-                ),
-              );
-            }
-          } 
-        );
-      },
-    );
-  }
-
-
-
-
-    // return ScopedModelDescendant<StockModel>(builder: (context, child, stockModel){
-    //   return FutureBuilder(
-    //     future: stockModel.futureStock,
-    //     builder: (BuildContext context, AsyncSnapshot<List<Stock>> snapshot){
-    //       switch(snapshot.connectionState){
-    //         case ConnectionState.none:
-    //           return Message.alert("Não foi possivel obter os dados necessários");
-    //         case ConnectionState.waiting:
-    //           return Message.loading(context, color: AppColors.primaryColor);
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+              return Message.alert("Não foi possivel obter os dados necessários");
+            case ConnectionState.waiting:
+              return Message.loading(context, color: AppColors.primaryColor);
             
-    //         default:
-    //           if(snapshot.hasError){
-    //             return Message.alert("Não foi possível obter os dados do servidor");
-    //           } else if(!snapshot.hasData){
-    //             return Message.alert("Não foi possivel obter os dados dos estoques");
-    //           } else if(snapshot.data.isEmpty){
-    //             return Message.alert("Nenhum estoque encontrado", fontSize: 16);
-    //           } else{
-    //               return RefreshIndicator(
-    //                 onRefresh: () async{
-    //                   this._reload();
-    //                 },
-    //                 child: ListView.builder(
-    //                   padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
-    //                   itemCount: snapshot.data.length,
-    //                   itemBuilder: (BuildContext context, int index){
-    //                     return StockTile(snapshot.data[index]);
-    //                   },
-    //                 ),
-    //               );
-    //           }
-    //       }
-    //     }
-    //   );
-    // });
+            default:
+              if(snapshot.hasError){
+                return Message.alert("Não foi possível obter os dados do servidor");
+              } else if(!snapshot.hasData){
+                return Message.alert("Não foi possivel obter os dados dos estoques");
+              } else if(snapshot.data.isEmpty){
+                return Message.alert("Nenhum estoque encontrado", fontSize: 16);
+              } else{
+                  return RefreshIndicator(
+                    onRefresh: () async{
+                      this._reload();
+                    },
+                    child: ListView.builder(
+                      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return StockTile(snapshot.data[index], _reload);
+                      },
+                    ),
+                  );
+              }
+          }
+        }
+      );
+    });
   }
+}
