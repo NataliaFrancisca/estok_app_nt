ba4be8608a4e2632e2b2a4d78f9cf926e41b0372
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-09 17:14:00 -0300

    feat: stock validator

---
+++ lib/ui/validator/stock_validator.dart
@@ -0,0 +1,53 @@
+import 'package:estok_app_natalia_francisca/ui/utils/format_date.dart';
+
+final messageIsEmpty = (String inputLabel) => 'O campo $inputLabel precisa ser preenchido';
+
+class StockValidator{
+  String validateDescription(String value){
+    if(value.isEmpty){
+      return messageIsEmpty('descrição');
+    }
+
+     if(value.length < 3){
+      return 'O campo deve ter no mínimo 3 caracteres';
+    }
+  }
+
+  String validateEntryDate(String entryDateString, String expirationDateString){
+    if(entryDateString.isEmpty){
+      return messageIsEmpty('data de entrada');
+    }
+
+    if(entryDateString.isNotEmpty && expirationDateString.isNotEmpty){
+      DateTime entryDate = tranformStringDate(entryDateString);
+      DateTime expirationDate = tranformStringDate(expirationDateString);
+
+      if(entryDate.isAfter(expirationDate)){
+        return 'A data de entrada não pode ser depois da data de validade';
+      }
+
+      if(entryDate.isAtSameMomentAs(expirationDate)){
+        return 'A data de entrada não pode ser no mesmo dia da validade';
+      }
+    }
+  }
+
+  String validateExpirationDate(String entryDateString, String expirationDateString){
+     if(entryDateString.isEmpty){
+      return messageIsEmpty('data de validade');
+    }
+
+    if(entryDateString.isNotEmpty && expirationDateString.isNotEmpty){
+      DateTime entryDate = tranformStringDate(entryDateString);
+      DateTime expirationDate = tranformStringDate(expirationDateString);
+
+      if(entryDate.isAfter(expirationDate)){
+        return 'A data de validade não pode ser antes da data de entrada';
+      }
+
+      if(entryDate.isAtSameMomentAs(expirationDate)){
+        return 'A data de validade não pode ser no mesmo dia da data de entrada';
+      }
+    }
+  }
+}
