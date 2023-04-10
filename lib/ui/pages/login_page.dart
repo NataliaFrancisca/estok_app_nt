f6bf56c04f1c8da0f8970d9c0fd5facb5890977c
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-10 13:08:34 -0300

    feat: fixing the problem with focus and adding the form

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -15,7 +15,9 @@ class _LoginPageState extends State<LoginPage> with LoginValidator{
   final _loginController = TextEditingController();
   final _senhaController = TextEditingController();
 
+  final FocusNode _focusLogin = FocusNode();
   final FocusNode _focusSenha = FocusNode();
+
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -23,94 +25,99 @@ class _LoginPageState extends State<LoginPage> with LoginValidator{
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      body: Form(
-        key: _formKey,
-        child: Container(
-          padding: const EdgeInsets.all(26.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              padding: const EdgeInsets.all(26.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
 
-            children: [
-              SizedBox(height: 20.0),
+                children: [
+                  SizedBox(height: 20.0),
 
-              Text(
-                "ESTOK APP",
-                style: TextStyle(
-                  
-                  fontWeight: FontWeight.w400,
-                  fontSize: 34,
-                  color: AppColors.blackTextColor,
-                ),
-              ),
+                  Text(
+                    "ESTOK APP",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 34,
+                      color: AppColors.blackTextColor,
+                    ),
+                  ),
 
-              SizedBox(height: 23.0),
+                  SizedBox(height: 23.0),
 
-              Text(
-                "Login",
-                style: TextStyle(
-                  
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                  color: AppColors.blackTextColor,
-                ),
-              ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: AppColors.blackTextColor,
+                    ),
+                  ),
 
-              SizedBox(height: 54.0),
+                  SizedBox(height: 54.0),
 
-              CustomTextFormField(
-                controller: _loginController,
-                labelText: 'E-mail',
-                hintText: 'Email',
-                keyboardType: TextInputType.emailAddress,
-                inputIcon: Icon(Icons.person, size: 22, color: AppColors.iconPrimaryColor),
-                requestFocus: _focusSenha,
-                validator: validateLogin
-              ),
+                  CustomTextFormField(
+                    controller: _loginController,
+                    labelText: 'E-mail',
+                    hintText: 'Email',
+                    keyboardType: TextInputType.emailAddress,
+                    inputIcon: Icon(Icons.person, size: 22, color: AppColors.iconPrimaryColor),
+                    validator: validateLogin,
+                    focusNode: _focusLogin,
+                    nextFocus: _focusSenha,
+                    previousFocus: _focusLogin,
+                  ),
 
-              SizedBox(height: 20.0),
+                  SizedBox(height: 20.0),
 
-              CustomTextFormField(
-                controller: _senhaController,
-                labelText: 'Senha',
-                hintText: 'Senha',
-                obscureText: true,
-                keyboardType: TextInputType.visiblePassword,
-                inputIcon: Icon(Icons.lock, size: 22, color: AppColors.iconPrimaryColor),
-                requestFocus: _focusSenha,
-                validator: validateSenha
-              ),
+                  CustomTextFormField(
+                    controller: _senhaController,
+                    labelText: 'Senha',
+                    hintText: 'Senha',
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    inputIcon: Icon(Icons.lock, size: 22, color: AppColors.iconPrimaryColor),
+                    focusNode: _focusSenha,
+                    previousFocus: _focusLogin,
+                    validator: validateSenha
+                  ),
 
-              SizedBox(height: 38.0),
+                  SizedBox(height: 38.0),
 
-              SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: (){
-                       _loginOnPressed(context);
-                      },
-                      child: Text(
-                          'Entrar'.toUpperCase(),
-                        style: TextStyle(fontSize: 15,
-                        color: AppColors.blackTextColor)
-                      ),
-
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: (){
+                           _loginOnPressed(context);
+                          },
+                          child: Text(
+                              'Entrar'.toUpperCase(),
+                            style: TextStyle(fontSize: 15,
+                            color: AppColors.blackTextColor)
                           ),
-                        ),
-                        elevation: MaterialStateProperty.all(0.0),
-                        padding: MaterialStateProperty.all(EdgeInsets.all(10)),
-                        backgroundColor: MaterialStateProperty.all(new Color(0xFFF7F2F8)),
+
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            ),
+                            elevation: MaterialStateProperty.all(0.0),
+                            padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                            backgroundColor: MaterialStateProperty.all(new Color(0xFFF7F2F8)),
+                          )
                       )
-                  )
+                  ),
+                ],
+              )
               ),
-            ],
-          )
           )
 
+        ),
       )
     );
   }
@@ -122,27 +129,24 @@ class _LoginPageState extends State<LoginPage> with LoginValidator{
     }
 
     UserModel.of(context).login(_loginController.text, _senhaController.text,
-        onSuccess: () {
-          Message.onSuccess(
-              scaffoldKey: _scaffoldKey,
-              message: "Usuário logado com sucesso!",
-              onPop: (value) {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context){
-                //   return HomePage();
-                // }));
-              }
-          );
-          return;
-        }, onFail: (String message) {
-          Message.onFail(
-              scaffoldKey: _scaffoldKey,
-              message: message,
-              onPop: (value){
-                print("Algo de errado com o login!");
-              }
-          );
-          return;
-        });
+      onSuccess: () {
+        Message.onSuccess(
+            scaffoldKey: _scaffoldKey,
+            message: "Usuário logado com sucesso!",
+            onPop: (value) {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+            }
+        );
+        return;
+      }, onFail: (String message) {
+        Message.onFail(
+            scaffoldKey: _scaffoldKey,
+            message: message,
+            onPop: (value){
+              print("Algo de errado com o login!");
+            }
+        );
+        return;
+      });
   }
 }
