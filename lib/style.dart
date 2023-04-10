af67c57fb31692875c3a315438ed0c23d56a652c
Natalia Francisca <nathifrancisca@gmail.com>
2023-04-10 16:01:23 -0300

    feat: delete file

--- lib/style.dart
+++
@@ -1,8 +0,0 @@
-import 'package:estok_app_natalia_francisca/colors.dart';
-import 'package:flutter/material.dart';
-
-final TextStyle customNavigationTextStyle = TextStyle(
-  fontWeight: FontWeight.w400,
-  fontSize: 16,
-  color: AppColors.primaryColor
-);
